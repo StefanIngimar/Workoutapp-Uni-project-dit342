52d64281555eb6712493ad31a63e141f53afd3c3
Alvuk <vuckovic@student.chalmers.se>
2024-09-12 12:37:59 +0200

    Updated POST api with email validator

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,13 @@
+//Dependencies
 var express = require('express');
 var router = express.Router();
+/*
+Express validator to verify email from: 
+"https://express-validator.github.io/docs/guides/getting-started/"
+*/ 
+const {body, validationResult} = require('express-validator');
 
+// Uses user model
 var User = require('../models/userModel');
 
 // Return all Users from database
@@ -26,11 +33,12 @@ router.get('/api/users/:id', async function(req, res, next){
 });
 
 // Return user in database by Username
-// Maybe not necessary?
 router.get('/api/users/:userName', async function(req, res){
-    var userName = req.params.userName;
+    var userName = req.params.userName; //extract userName from URL parameter
     try{
+        // Queries database for user that matches URL
         const aUser = await User.find({userName: userName});
+        // If query is succesful respond to client with status code 200 and send back requested user as JSON
         res.status(200).json(aUser);
     }catch(err){
         res.status(404).send(err);
@@ -52,13 +60,23 @@ router.delete('/api/users/:id', async function(req, res){
 
 
 // Creation of a new User
-router.post('/api/users', async function(req, res){
-    var user = new User({
-        'userName'   : req.body.userName,
-        'email'      : req.body.email,
-        'password'   : req.body.password,
-        'profilePic' : req.body.profilePic
-    });
+router.post('/api/users',
+    [
+        body('email').isEmail().withMessage('Enter a valid email in the format xxx@xxx.xxx')
+    ], 
+    async function(req, res){
+        const queryResult = validationResult(req);
+        if (queryResult.isEmpty()){
+            var user = new User({
+                'userName'   : req.body.userName,
+                'email'      : req.body.email,
+                'password'   : req.body.password,
+                'profilePic' : req.body.profilePic
+            });
+        } else {
+            return res.status(400).json({errors: queryResult.array()});
+
+        }
     try{
         const savedUser = await user.save();
         res.status(201).json(savedUser);
