75602821071caaf6fb83e9110e6ea8aa6d3095fc
stefanla <stefanla@student.chalmers.se>
2024-09-16 15:30:39 +0200

    v1 api

--- server/controllers/leaderboardController.js
+++ server/controllers/leaderboardController.js
@@ -2,21 +2,22 @@ const express = require('express');
 const router = express.Router();
 const Leaderboard = require('../models/leaderboardModel.js');
 
-router.get('/api/leaderboard', async function(req, res){
+router.get('/api/v1/leaderboard', async function(req, res){
     try {
         const allLeaderboard = await Leaderboard.find({});
         res.status(200).json(allLeaderboard)
     } catch(err) {
         res.status(404).send}});
 
-router.get('/api/leaderboard/:id', async function(req, res){
+router.get('/api/v1/leaderboard/:id', async function(req, res){
     var id = req.params.id;
     try {
-        const leaderboard = await Leaderboard.findById(id);}
+        const leaderboard = await Leaderboard.findById(id);
+        res.status(200).json(leaderboard);}
     catch(err) {
         res.status(404).send(err)};});
 
-router.get('/api/leaderboard/:userid', async function(req, res){
+router.get('/api/v1/leaderboard/:userid', async function(req, res){
     var userid = req.params.userid;
     try{
         const leaderboard = await Leaderboard.find({user: userid});
@@ -24,17 +25,18 @@ router.get('/api/leaderboard/:userid', async function(req, res){
     catch(err){
         res.status(404).send(err);}});
 
-router.post('/api/leaderboard/newleaderboard', async function(req, res){
+router.post('/api/v1/leaderboard', async function(req, res){
     try {
         var leaderboard = new Leaderboard({
             'user' : req.body.user,
-            'weight' : req.body.weight});
+            'weight' : req.body.weight
+        });
         const savedLeaderboard = await leaderboard.save();
         res.status(200).send(savedLeaderboard);}
     catch(err){
         res.status(500).send(err);}});
 
-router.put('/api/leaderboard/:userid', async function(req, res){
+router.put('/api/v1/leaderboard/:userid', async function(req, res){
     var userid = req.params.userid;
     try{
         let leaderboard = await Leaderboard.findOneAndUpdate({user: userid}, req.body);
@@ -47,12 +49,16 @@ router.put('/api/leaderboard/:userid', async function(req, res){
                 res.status(500).send(err);
         }});
 
-router.delete('/api/leaderboard/:userid', async function(req, res) {
+router.delete('/api/v1/leaderboard/:userid', async function(req, res) {
     var userid = req.params.userid;
     try{
-        const leaderboard = await Leaderboard.findByIdAndDelete({user: userid});
-        res.status(200).send({leaderboard, message: "User successfully deleted from leaderboard"});}
-    catch(err){
-        res.status(500).send(err);}});
+        const leaderboard = await Leaderboard.findOneAndDelete({user: userid});
+        if(leaderboard){
+            res.status(200).send({leaderboard, message: "Leaderboard entry successfully deleted"});
+        } else{
+            res.status(404).send({message: "Leaderboard entry not found"});
+        }
+    }catch(err){
+            res.status(500).send(err);}});
 
 exports = module.exports = router;
--- server/models/leaderboardModel.js
+++ server/models/leaderboardModel.js
@@ -4,8 +4,7 @@ var Schema = mongoose.Schema;
 
 var leaderboardSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: 'users' },
-    workoutLog: { type: Schema.Types.ObjectId, ref: 'workoutLogModel' }
+    weight: { type: Number }
 });
-//need to fix this and get the actual weight instead of the entire object
 
-module.exports = mongoose.model('Leaderbord', leaderboardSchema);
+module.exports = mongoose.model('Leaderboard', leaderboardSchema);
