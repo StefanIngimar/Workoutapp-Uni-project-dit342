5382ad1bceec9adc58a88190bc7878809dca1914
stefanla <stefanla@student.chalmers.se>
2024-09-26 17:31:02 +0200

    code does not work, just trying to get calendar to work

--- client/src/views/Workoutlogs.vue
+++ client/src/views/Workoutlogs.vue
@@ -1,15 +1,40 @@
-<template>
-    <h1> Workoutlog </h1>
-</template>
-
 <script>
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import axios from 'axios';
+//code is not working, just trying to fetch data properly
+//https://fullcalendar.io/docs/events-json-feed
 export default {
-    name: 'workoutlogs'
+  components: {
+    FullCalendar
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [dayGridPlugin, interactionPlugin],
+        initialView: 'dayGridMonth',
+        eventSources: [
+          {
+            url: '/api/v1/workoutlogs',
+            method: 'GET',
+            extraParams: {
+              custom_param1: 'something',
+              custom_param2: 'somethingelse'
+            },
+            failure: function() {
+              alert('There was an error while fetching workout logs!');
+            },
+            color: 'yellow',
+            textColor: 'black'
+          }
+        ]
+      }
+    }
+  }
 }
 </script>
 
-<style scoped>
-h1 {
-    color: blueviolet;
-}
-</style>
+<template>
+  <FullCalendar :options="calendarOptions" />
+</template>
