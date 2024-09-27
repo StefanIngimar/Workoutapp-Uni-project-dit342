3fa9a819ff23efcd1ed3f24ee2ebc65d037b8b58
gabjohn <gabjohn@student.chalmers.se>
2024-09-27 15:45:09 +0200

    Added more functionality to sessions

--- client/src/components/SessionItem.vue
+++ client/src/components/SessionItem.vue
@@ -2,25 +2,26 @@
   <div class="session-list">
     <div class="session-container">
       <div v-if="isEditing">
-        <p>Name: <input v-model="editSession.name" /></p>
+        <p>Name: <input v-model="editSession.sessionName" /></p>
         <p>Duration: <input type="number" v-model="editSession.duration" /></p>
         <p>Completed: <input type="checkbox" v-model="editSession.isCompleted" /></p>
         <p>Notes: <input v-model="editSession.notes" /></p>
         <p>Exercises:
         <div v-for="exercise in session.exercises" v-bind:key="exercise._id">
-          <exercise-item v-bind:exercise="exercise" />
+          <exercise-item v-bind:exercise="exercise" @exercise-deleted="handleExerciseDeleted"
+            @delete-error="handleDeleteError" @exercise-updated="handleExerciseUpdated" />
         </div>
         </p>
 
         <div v-if="isAddingExercise">
-          <p>Name: <input v-model="exercise.name" /></p>
-          <p>Bodypart: <input v-model="exercise.bodyPart" /></p>
-          <p>Bodyweight: <input type="checkbox" v-model="exercise.hasWeight" /></p>
-          <p>Weight: <input type="number" v-model="exercise.weight" /></p>
-          <p>Reps: <input type="number" v-model="exercise.reps" /></p>
-          <p>Sets: <input type="number" v-model="exercise.sets" /></p>
+          <p>Name: <input v-model="name" /></p>
+          <p>Bodypart: <input v-model="bodyPart" /></p>
+          <p>Bodyweight: <input type="checkbox" v-model="hasWeight" /></p>
+          <p>Weight: <input type="number" v-model="weight" /></p>
+          <p>Reps: <input type="number" v-model="reps" /></p>
+          <p>Sets: <input type="number" v-model="sets" /></p>
 
-          <b-button class="btn_message" variant="success" v-on:click="addNewExercise">Save</b-button>
+          <b-button class="btn_message" variant="success" v-on:click="addNewExercise">Submit</b-button>
           <b-button class="btn_message" variant="danger" v-on:click="toggleNewExercise">Cancel</b-button>
         </div>
 
@@ -32,17 +33,18 @@
 
       <div v-else>
 
-        <p>Name: {{ session.name }}</p>
+        <p>Name: {{ session.sessionName }}</p>
         <p>Completed: {{ session.isCompleted }}</p>
         <p>Duration: {{ session.duration }}</p>
         <p>Notes: {{ session.notes }}</p>
         <p>Exercises:
         <div v-for="exercise in session.exercises" v-bind:key="exercise._id">
-          <exercise-item v-bind:exercise="exercise" />
+          <exercise-item v-bind:exercise="exercise" @exercise-deleted="handleExerciseDeleted"
+            @delete-error="handleDeleteError" @exercise-updated="handleExerciseUpdated" />
         </div>
         </p>
-
-        <b-button class="btn_message" variant="danger" v-on:click="deleteSession(session._id)">X</b-button>
+        <p> {{ exerciseMessage }}</p>
+        <b-button class="btn_message" variant="danger" v-on:click="deleteSession(this.session._id)">X</b-button>
         <b-button class="btn_message" variant="primary" v-on:click="toggleEdit">Edit</b-button>
       </div>
     </div>
@@ -65,7 +67,15 @@ export default {
       isAddingExercise: false,
       isEditing: false,
       exercise: '',
-      editSession: { ...this.session }
+      editSession: { ...this.session },
+      exerciseMessage: '',
+      name: '',
+      hasWeights: false,
+      weight: '',
+      bodyPart: '',
+      isCustom: true,
+      reps: '',
+      sets: '',
     };
   },
   methods: {
@@ -78,7 +88,7 @@ export default {
       this.editSession = { ...this.session };
     },
     saveSession() {
-      Api.patch(`/v1/session/${this.session._id}`, this.editSession)
+      Api.patch(`/v1/dailysessions/${this.session._id}`, this.editSession)
         .then((response) => {
           this.isEditing = false;
           this.$emit('session-updated', response.data);
@@ -87,8 +97,8 @@ export default {
           console.error('Error saving session:', error);
         });
     },
-    deleteSession(exerciseID) {
-      Api.delete(`/v1/session/${exerciseID}`)
+    deleteSession(sessionID) {
+      Api.delete(`/v1/dailysessions/${sessionID}`)
         .then((response) => {
           this.$emit('session-deleted', response.data);
         })
@@ -97,7 +107,16 @@ export default {
         });
     },
     addNewExercise() {
-      Api.post(`/v1/session/${this.session._id}/exercise`, this.exercise)
+      Api.post(`/v1/dailysessions/${this.session._id}/exercises`,
+        {
+          name: this.name,
+          hasWeights: this.hasWeights,
+          weight: this.weight,
+          bodyPart: this.bodyPart,
+          isCustom: this.isCustom,
+          reps: this.reps,
+          sets: this.sets
+        })
         .then((response) => {
           this.isAddingExercise = false;
           this.$emit('session-updated', response.data);
@@ -108,7 +127,30 @@ export default {
     },
     toggleNewExercise() {
       this.isAddingExercise = !this.isAddingExercise;
-    }
+    },
+    handleExerciseUpdated() {
+      Api.get(`/v1/dailysessions/${this.session._id}/exercises`)
+        .then((response) => {
+          this.session.exercises = response.data;
+        })
+        .catch((error) => {
+          this.exerciseMessage = error;
+        })
+    },
+
+    handleExerciseDeleted() {
+      Api.get(`/v1/dailysessions/${this.session._id}/exercises`)
+        .then((response) => {
+          this.session.exercises = response.data;
+          this.exerciseMessage = "Exercise deleted!";
+        })
+        .catch((error) => {
+          this.exerciseMessage = error;
+        })
+    },
+    handleDeleteError() {
+      this.exerciseMessage = error;
+    },
   }
 };
 </script>
--- client/src/views/Sessions.vue
+++ client/src/views/Sessions.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
     <h1> Sessions </h1>
-    <session-item v-bind:exercise="message" />
+    <div v-if="sessionMessage">
+      <session-item v-bind:session="sessionMessage" @session-deleted="handleSessionDeleted"
+        @delete-error="handleDeleteError" @session-updated="handleSessionUpdated" />
+    </div>
+
+
+    <div class="form">
+      <p>Name: <input v-model="sessionName" /></p>
+      <p>Duration: <input type="number" v-model="duration" /></p>
+      <p>Completed: <input type="checkbox" v-model="isCompleted" /></p>
+      <p>Notes: <input v-model="notes" /></p>
+      <p>Exercises:</p>
+
+      <b-button class="btn_message" variant="primary" v-on:click="postSession()">Submit session</b-button>
+    </div>
 
     <div v-for="session in sessions" v-bind:key="session._id">
-      <session-item v-bind:session="session" />
+      <session-item v-bind:session="session" @session-deleted="handleSessionDeleted" @delete-error="handleDeleteError"
+        @session-updated="handleSessionUpdated" />
     </div>
   </div>
 
@@ -22,11 +37,6 @@ export default {
     MyFooter,
     SessionItem
   },
-  data() {
-    return {
-      message: 'none'
-    }
-  },
   mounted() {
     Api.get('/v1/dailysessions')
       .then((response) => {
@@ -38,13 +48,76 @@ export default {
   },
   methods: {
 
+    handleSessionUpdated() {
+      Api.get('/v1/dailysessions')
+        .then((response) => {
+          this.sessions = response.data;
+        })
+        .catch((error) => {
+          this.exerciseMessage = error;
+        })
+    },
+
+    handleSessionDeleted() {
+      Api.get('/v1/dailysessions')
+        .then((response) => {
+          this.sessions = response.data;
+          this.sessionMessage = '';
+        })
+        .catch((error) => {
+          this.sessionMessage = error;
+        })
+    },
+    handleDeleteError() {
+      this.sessionMessage = error;
+    },
+    postSession() {
+      Api.post('/v1/dailysessions',
+        {
+          userID: this.userID,
+          sessionName: this.sessionName,
+          duration: this.duration,
+          isCompleted: this.isCompleted,
+          notes: this.notes,
+          exercises: ''
+        }
+      )
+        .then((response) => {
+          this.sessionMessage = response.data;
+          Api.get('/v1/dailysessions')
+            .then((response) => {
+              this.sessions = response.data
+            })
+            .catch((error) => {
+              this.sessionsMessage = error
+            })
+        })
+        .catch((error) => {
+          this.sessionMessage = error;
+        })
+    }
   },
   data() {
     return {
-      sessions: ''
+      sessions: '',
+      message: 'none',
+      postMessage: '',
+      sessionMessage: '',
+      userID: '',
+      sessionName: '',
+      duration: '',
+      isCompleted: false,
+      notes: ''
     }
   }
 }
 </script>
 
-<style></style>
+<style scoped>
+.form {
+  margin-left: 800px;
+  margin-right: 800px;
+  padding: 50px;
+  background-color: rgb(125, 190, 221);
+}
+</style>
